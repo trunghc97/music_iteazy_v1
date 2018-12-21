c02d36c1fa5c372eb2e7cfa96c221b92fc219416
TrungHC <trunghuynh060597@gmail.com>
2018-12-21 22:04:40 +0700

    Refer to Task #4670: Upload Song Cloudinary

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
-  SONG_ATTRIBUTES = %i(title lyrics song_url view img_url user_id)
+  SONG_ATTRIBUTES = %i(singer_id title lyrics song_url view img_url user_id)
                     .push(singer_attributes: %i(name description)).freeze
 
   belongs_to :user
@@ -11,13 +11,31 @@ class Song < ApplicationRecord
   has_many :genre_songs, dependent: :destroy
   has_many :genres, through: :genre_songs
 
+  mount_uploader :img_url, ImgUrlUploader
+  mount_uploader :song_url, SongUrlUploader
+
   validates :title, presence: true,
     length: {maximum: Settings.title.max_length}
-  validates :song_url, presence: true,
-    uniqueness: true
+  validates :song_url, presence: true
   validates :view, numericality: true
+  validate :img_size
+  validate :song_size
 
   scope :include_to_song, ->{includes :singer, :comments, :genres}
   scope :hot_feed, ->{order view: :desc}
   scope :search_song, ->(key_word){where("title LIKE ?", "%#{key_word}%")}
+
+  private
+
+  def img_size
+    if img_url.size > Settings.file_size.megabytes
+      errors.add :img_url, I18n.t(".out_of_size")
+    end
+  end
+
+  def song_size
+    if song_url.size > Settings.file_size.megabytes
+      errors.add :song_url, I18n.t(".out_of_size")
+    end
+  end
 end
